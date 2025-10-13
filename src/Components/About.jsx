c3515dde99fb2a04392ef191309be3a9a725fdcc
naveenkumar-rm <naveenkmayilsamy@gmail.com>
2025-10-13 14:56:55 +0530

    delete some content in about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,10 +5,9 @@ const About = () => {
     <div id='About' className='about'>
       <h1 className='Skills_heading'>About me</h1>
       <div className='about_description'>
-      <p>Hello! I’m Naveen Kumar, a passionate and detail-oriented web developer from Coimbatore, Tamil Nadu. I hold a Bachelor’s degree in Commerce with Computer Applications, which has given me a strong foundation in both business concepts and computer technologies.</p>
-      <p> began my professional journey with Cognizant Technology Solutions, where I gained valuable industry exposure and developed strong communication, teamwork, and problem-solving skills.</p>
-      <p>Currently, I’m interning at GT Software, where I’m enhancing my expertise in web development by working on real-world projects and honing my skills in creating responsive, efficient, and visually appealing websites.</p>
-      <p>I enjoy turning ideas into functional digital experiences by combining creativity with technology. Whether it’s crafting intuitive user interfaces or optimizing website performance, I aim to build solutions that are both impactful and user-friendly. Outside of coding, I’m constantly exploring new tools, frameworks, and design trends to stay ahead in this ever-evolving tech landscape.</p>
+      <p>Hello! I’m Naveen Kumar, a passionate and detail-oriented web developer from Coimbatore, Tamil Nadu.</p>
+      <p>Currently, I’m interning at GT Software, where I’m enhancing my expertise in Full stack development by working on real-world projects and honing my skills in creating responsive, efficient, and visually appealing websites.</p>
+      <p>Outside of coding, I’m constantly exploring new tools, frameworks, and design trends to stay ahead in this ever-evolving tech landscape.</p>
     </div>
     </div>
       )
